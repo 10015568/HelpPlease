9aa4490ff5a09307f8c0aaa75303d00b6ad30d02
10015568 <10015568@sbstudents.org>
2022-03-16 17:34:41 -0400

    Sydney added comments

--- VRGitDemo.java
+++ VRGitDemo.java
@@ -17,7 +17,13 @@ public class VRGitDemo
 		//QUOTIENT
 		Quotient q = (float a, float b) -> System.out.printf("%.3f", a/b);
 		q.quo(5f, 2f);
-
+		/*
+			SYDNEY
+			I have a better understanding of how repositories and GitKraken work, and I know how to make
+			branches and merge them. In addition, I am aware of how to push and pull new updates to and from a
+			partner. Communication was also a vital part in completing this project because Anisha and I
+			both ran into problems and had to collaborate to figure out how to fix them.
+		*/
 		//PERFECT SQUARE
 		PerfectSquare s = (int num) -> Math.sqrt(num) == Math.floor(Math.sqrt(num));
 		System.out.println(s.test(144));
